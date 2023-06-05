e316a6b8de0f37bee9ab6e0f36f60567f3a6f326
Alfredo08 <alfredosv08@gmail.com>
2023-06-05 09:27:46 -0600

    Fundamentos de python

---
+++ Semana 1/Día 1/tuplas.py
@@ -0,0 +1,12 @@
+nombres = ( 'Alejandro', 'Martha', 'Roger', 'Julieta', 'Lucia', 'Fernanda', 'Alan' )
+
+print( nombres )
+print( nombres[2] )
+
+# nombres[0] = 'Alex'
+# nombres.append( 'Alfredo' )
+
+estudiante = ( 'Alex', 'Morales', 25, True )
+print( estudiante )
+
+
